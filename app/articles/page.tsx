d7daeeead96215ef6cb8db8667eed2b1809598c5
Jaha  Naeem Gitonga <jahagitonga@Jahas-MacBook-Pro.local>
2023-05-29 00:03:36 -0400

    fix: add link to medium

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -5,7 +5,7 @@ export default function Blog(): JSX.Element {
   return (
     <div>
       <p className="textAlign">
-        Coming Soon! My plans are to host my own articles here first.
+        Coming Soon! My plans are to host my own articles here first versus where they currently are, <a href="https://naeemgtng.medium.com/" target="_blank">Medium</a>.
       </p>
       <div className="construction" />
       <Footer />
