b3a5a9a7e80778fe1fa7f16dd81a6a19b670bec9
Jaha  Naeem Gitonga <jahagitonga@Jahas-MacBook-Pro.local>
2024-03-19 20:32:11 -0400

    feat: add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,24 @@ export default function RootLayout({
           rel="shortcut icon"
           href="https://d2j3yisnywcb30.cloudfront.net/pix/favicon.ico"
         />
+
+        <Script type="text/javascript">
+          {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PF892R2H');
+          `}
+        </Script>
       </head>
       <body id="body" className="body">
+
+        <noscript>
+          <iframe className="iframe" src="https://www.googletagmanager.com/ns.html?id=GTM-PF892R2H"
+        height="0" width="0"></iframe>
+        </noscript>
+
         {children}
         <Script
           id="font-awesome"
