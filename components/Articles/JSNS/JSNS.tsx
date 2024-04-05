b4889a7414b26422c16ec0ddfe5284fd9c7baede
Jaha  Naeem Gitonga <jahagitonga@Jahas-MacBook-Pro.local>
2024-04-04 23:29:51 -0400

    feat: added javascript's new sexy

--- components/Articles/JSNS/JSNS.tsx
+++ components/Articles/JSNS/JSNS.tsx
@@ -9,18 +9,17 @@ import Tags from '@/components/Tags/Tags';
 import newSexy from '../../pictures/js-new.png';
 import { imageLoader } from '@/utils/imageLoader';
 import ReturnArrow from '@/components/ReturnArrow/ReturnArrow';
+import { articles } from '@/utils/articles';
 
 export default function JSNS(): JSX.Element {
   const { innerWrapper, imageWrapper, altText, text, code, pre, subtext } =
     styles;
   const { tenPadding, width75, minus10LeftMargin } = sharedStyles;
-
+    const article = articles[4];
   return (
     <div id="jsns" className={`${tenPadding}`}>
       <PageHeader headerName="article" hideLinks={false} />
       <div className={`${width75} ${innerWrapper}`}>
-        <h1>JavaScript&apos;s New Sexy</h1> {/** &rdquo; &ldquo; */}
-        <p className={subtext}>TypeScript: The Developer&apos;s Tool</p>
         <div className={imageWrapper}>
           <Image
             alt="Teaching student"
@@ -30,7 +29,9 @@ export default function JSNS(): JSX.Element {
             fill
           />
           <p className={altText}>Photo by NESA by Makers on Unsplash</p>
-        </div>
+        </div><h1>JavaScript&apos;s New Sexy</h1> {/** &rdquo; &ldquo; */}
+        <p className={subtext}>TypeScript: The Developer&apos;s Tool</p>
+        
         <p className={text}>
           If you&apos;re new to TypeScript, beware, it&apos;s not without its
           critics. Just two years shy of being 10 years old, it&apos;s been
