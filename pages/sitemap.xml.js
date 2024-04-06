b278c78fd569459984be4a2fb3d6deaae93c0995
Jaha  Naeem Gitonga <jahagitonga@Jahas-MacBook-Pro.local>
2024-04-06 01:45:48 -0400

    fix: update generate sitemap.xml call

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,11 +1,11 @@
 const EXTERNAL_DATA_URL = 'https://jahanaeemgitonga.com/articles';
 
-function generateSiteMap(posts) {
+function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
        <loc>https://jahanaeemgitonga.com</loc>
-       <priority>1</priority>
+       <priority>1.0</priority>
        <lastmod>2024-04-01</lastmod>
      </url>
      <url>
@@ -69,12 +69,7 @@ function SiteMap() {
 }
 
 export async function getServerSideProps({ res }) {
-  // We make an API call to gather the URLs for our site
-  const request = await fetch(EXTERNAL_DATA_URL);
-  const posts = await request.json();
-
-  // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts);
+  const sitemap = generateSiteMap();
 
   res.setHeader('Content-Type', 'text/xml');
   // we send the XML to the browser
