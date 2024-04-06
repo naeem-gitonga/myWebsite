32da92f924c2c89e80b8e814350a062e6fc9eaf8
Naeem <blackb8r@gmail.com>
2024-04-06 18:17:12 +0000

    Staging

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,6 +1,29 @@
-const EXTERNAL_DATA_URL = 'https://jahanaeemgitonga.com/articles';
+const products = require('../utils/products')['products'];
 
 function generateSiteMap() {
+  const articleEntries = [
+    'default-parameters',
+    'do-i-need-a-cka',
+    'explain-servers-to-5-year-old',
+    'i-am',
+    'js-new-sexy',
+    'rapidbackend',
+    'react-context-api',
+    'sam-lambda-mongodb',
+  ].map((file) => {
+    return `
+      <url>
+        <loc>https://jahanaeemgitonga.com/articles/${file}</loc>
+      </url>
+    `;
+  });
+  const productsEntries = products.map(
+    (p) => `
+    <url>
+      <loc>https://jahanaeemgitonga.com${p.productUrl}</loc>
+    </url>
+    `
+  );
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
@@ -9,63 +32,34 @@ function generateSiteMap() {
        <lastmod>2024-04-01</lastmod>
      </url>
      <url>
-       <loc>https://jahanaeemgitonga.com/articles/i-am</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/articles/default-parameters</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/articles/do-i-need-a-cka</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/articles/explain-servers-to-5-year-old</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/articles/js-new-sexy</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/articles/rapidbackend</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/articles/react-context-api</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/articles/sam-lambda-mongodb</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/shop</loc>
-     </url>
-     <url>
        <loc>https://jahanaeemgitonga.com/about</loc>
        <priority>0.9</priority>
        <lastmod>2024-04-06</lastmod>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/item?item_id=1</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/item?item_id=2</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/item?item_id=3</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/item?item_id=4</loc>
-     </url>
-     <url>
-       <loc>https://jahanaeemgitonga.com/item?item_id=4</loc>
+       <changefreq>monthly</changefreq>
      </url>
      <url>
        <loc>https://jahanaeemgitonga.com/work</loc>
        <lastmod>2024-04-06</lastmod>
+       <changefreq>yearly</changefreq>
      </url>
+     <url>
+       <loc>https://jahanaeemgitonga.com/articles</loc>
+       <lastmod>2024-04-06</lastmod>
+       <changefreq>weekly</changefreq>
+     </url>
+     <url>
+       <loc>https://jahanaeemgitonga.com/shop</loc>
+       <changefreq>yearly</changefreq>
+     </url>
+      ${articleEntries.join('')}
+      ${productsEntries.join('')}
    </urlset>
  `;
 }
 
 function SiteMap() {
   // getServerSideProps will do the heavy lifting
-  console.log('working')
+  console.log('working');
 }
 
 export async function getServerSideProps({ res }) {
