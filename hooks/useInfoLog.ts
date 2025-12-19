e06d73cd283230224f30f72cf0bfeffc9836c7fc
Naeem Gitonga <blackb8r@gmail.com>
2025-12-19 14:31:21 -0500

    chore: update copy in terminal log

--- hooks/useInfoLog.ts
+++ hooks/useInfoLog.ts
@@ -4,7 +4,7 @@ export default function useInfoLog(): void {
   useEffect(() => {
     console.log('%cHi Everyone!', 'color: lime; font-size: 50px');
     console.log(
-      "%cHey if you're reading this you should hire me to build your next website, web or mobile app. I am an awesome engineer! So instead of snooping around in the console, call me! You can reach me at 404.670.0059. Leave a message or text if I don't answer. --Naeem",
+      "%cLife is short, so make it good. Love yourself, do for others, and take it but take lite. --Naeem",
       'color: lime; font-size: 30px'
     );
   }, []);
