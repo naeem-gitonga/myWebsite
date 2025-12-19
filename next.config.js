6a971a9d474a95164368e59d602fa38c3589bd7d
Naeem Gitonga <blackb8r@gmail.com>
2025-12-18 23:54:45 -0500

    fix: make images webp and load super fast

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    formats: ['image/webp', 'image/avif'],
+    minimumCacheTTL: 31536000,
     remotePatterns: [
       {
         protocol: 'https',
