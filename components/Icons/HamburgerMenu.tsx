2e06e73d442f8ed0ad5f79f53d5102e598c52ff1
Naeem Gitonga <blackb8r@gmail.com>
2026-01-16 12:14:50 -0500

    feat: add hamburger menu

---
+++ components/Icons/HamburgerMenu.tsx
@@ -0,0 +1,17 @@
+type HamburgerMenuProps = {
+  fill: string;
+};
+export default function HamburgerMenu(props: HamburgerMenuProps): React.JSX.Element {
+  const { fill } = props;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill={fill}
+      width="100%"
+      height="100%"
+      viewBox="0 0 448 512"
+    >
+      <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+    </svg>
+  );
+}
