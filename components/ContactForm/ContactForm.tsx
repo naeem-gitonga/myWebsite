861e5b85461a346a844e6694baf0a4d4d0cddb86
Naeem Gitonga <blackb8r@gmail.com>
2026-02-21 18:24:52 -0500

    feat: update the contact form

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,16 +1,13 @@
 'use client';
 import styles from './ContactForm.module.scss';
-import sharedStyles from '../SharedCss/SharedCss.module.scss';
 import PageHeader from '../PageHeader/PageHeader';
 
 export default function ContactForm(): React.JSX.Element {
-  const { contactWrapper, contactForm, input, form, message, submit, label } =
+  const { contactWrapper, contactForm, form, fieldGroup, input, message, submit, label } =
     styles;
+
   return (
-    <div
-      id="contact"
-      className={`${contactWrapper} ${sharedStyles.height100vh}`}
-    >
+    <div id="contact" className={contactWrapper}>
       <PageHeader headerName="contactMe" hideLinks={false} />
       <div className={contactForm}>
         <form
@@ -18,37 +15,46 @@ export default function ContactForm(): React.JSX.Element {
           action={process.env.NEXT_PUBLIC_FORM_URL}
           method="POST"
         >
-          <label className={label} htmlFor="contact-form-name">
-            Your Name
-          </label>
-          <input
-            className={input}
-            type="text"
-            name="name"
-            id="contact-form-name"
-            placeholder="Your Name"
-            required
-          />
-          <label className={label} htmlFor="contact-form-email">
-            Your Email Address
-          </label>
-          <input
-            className={input}
-            type="email"
-            name="_replyto"
-            id="contact-form-email"
-            placeholder="grace@example.com"
-            required
-          />
-          <label className={label} htmlFor="contact-form-message">
-            Message
-          </label>
-          <textarea
-            name="message"
-            className={message}
-            id="contact-form-message"
-            required
-          ></textarea>
+          <div className={fieldGroup}>
+            <label className={label} htmlFor="contact-form-name">
+              Your Name
+            </label>
+            <input
+              className={input}
+              type="text"
+              name="name"
+              id="contact-form-name"
+              placeholder="Your Name"
+              required
+            />
+          </div>
+
+          <div className={fieldGroup}>
+            <label className={label} htmlFor="contact-form-email">
+              Your Email Address
+            </label>
+            <input
+              className={input}
+              type="email"
+              name="_replyto"
+              id="contact-form-email"
+              placeholder="grace@example.com"
+              required
+            />
+          </div>
+
+          <div className={fieldGroup}>
+            <label className={label} htmlFor="contact-form-message">
+              Message
+            </label>
+            <textarea
+              name="message"
+              className={message}
+              id="contact-form-message"
+              required
+            />
+          </div>
+
           <input className={submit} type="submit" value="Send message" />
         </form>
       </div>
