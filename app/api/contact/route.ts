566284f2553f1cf06c3922022bf551672bfadd12
Naeem Gitonga <blackb8r@gmail.com>
2026-02-22 01:46:03 -0500

    refactor: use vercel env

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,10 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const FORM_URL = process.env.VERCEL_ENV === 'production'
+  ? 'https://ney0s44h42.execute-api.us-east-1.amazonaws.com/prod/api/ngcontact'
+  : 'https://ans9ms0e2h.execute-api.us-east-1.amazonaws.com/prod/api/ngcontact-staging';
+
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
-    const res = await fetch(process.env.FORM_URL as string, {
+    const res = await fetch(FORM_URL, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
